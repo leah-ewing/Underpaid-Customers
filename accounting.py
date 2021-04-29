08d0c13c5d0cda445e01ed4601457cb47cb513c2
Leah Ewing <leahewing@live.com>
2021-04-29 22:34:55 +0000

    Debug Commit - won't loop through entire file currently

--- accounting.py
+++ accounting.py
@@ -5,17 +5,19 @@ def customer_data(file_path):
 
     reads file path and returns whether a customer has over or underpaid
     for their melons"""
-    data_report = open(file_path)
+    print('Melon Report')
+    data_report = open(file_path) #opens input file
+
     for line in data_report:
         line = line.rstrip()
         words = line.split('|') #splits words at the '|' symbol
 
-        customer_name = words[1]
-        customer_melons = words[2]
-        customer_paid = words[3]
+        customer_name = words[1] #sets customer name as second column in file
+        customer_melons = float(words[2]) #sets melon bought count as third column
+        customer_paid = float(words[3]) #sets customer paid amount as fourth column
 
-    customer_expected = int(customer_melons) * int(melon_cost)
-    if customer_expected != customer_paid:
-        print("{} paid ${}, expected ${}".format(customer_name, customer_melons, customer_paid))
+    customer_expected = customer_melons * melon_cost 
+    if customer_expected != customer_paid:  
+        print(f"{customer_name} paid ${customer_melons:.2f}, expected ${melon_cost:.2f}")
 
 customer_data("customer-orders.txt")
