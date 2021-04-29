7ca798c920376e1daf664aa3260fbdb3bdb030b9
Leah Ewing <leahewing@live.com>
2021-04-29 22:42:19 +0000

    Fixed Loop

--- accounting.py
+++ accounting.py
@@ -15,9 +15,8 @@ def customer_data(file_path):
         customer_name = words[1] #sets customer name as second column in file
         customer_melons = float(words[2]) #sets melon bought count as third column
         customer_paid = float(words[3]) #sets customer paid amount as fourth column
-
-    customer_expected = customer_melons * melon_cost 
-    if customer_expected != customer_paid:  
-        print(f"{customer_name} paid ${customer_melons:.2f}, expected ${melon_cost:.2f}")
+        customer_expected = customer_melons * melon_cost 
+        if customer_expected != customer_paid:  
+            print(f"{customer_name} paid ${customer_melons:.2f}, expected ${melon_cost:.2f}")
 
 customer_data("customer-orders.txt")
