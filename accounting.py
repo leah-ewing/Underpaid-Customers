575a8956c83ca6b00ca92fd5c474b2ab4a802d17
Leah Ewing <leahewing@live.com>
2021-04-29 23:13:03 +0000

    Added Comments

--- accounting.py
+++ accounting.py
@@ -12,21 +12,20 @@ def customer_data(file_path):
         line = line.rstrip()
         words = line.split('|') #splits words at the '|' symbol
 
-        customer_name = words[1] #sets customer name as second column in file
-        customer_melons = float(words[2]) #sets melon bought count as third column
-        customer_paid = float(words[3]) #sets customer paid amount as fourth column
+        customer_name = words[1] #sets customer name as second column as file
+        customer_melons = float(words[2]) #sets amount of melons bought as third column and as a decimal number
+        customer_paid = float(words[3]) #sets amount customer paid as fourth column and as a decimal number
         customer_expected = customer_melons * melon_cost 
+        #sets amount customer expected to pay as the number of melons bought multiplied by the melon's cost
         
         if customer_expected > customer_paid:
-            print(f"{customer_name} *UNDERPAID!* paid: ${customer_melons:.2f}, expected ${melon_cost:.2f}")
+            print(f"{customer_name} *UNDERPAID!* paid: ${customer_paid:.2f}, expected ${customer_expected:.2f}")
+            #if statement checks if the expected amount is more than amount customer paid
         elif customer_expected < customer_paid:
-            print(f"{customer_name} OVERPAID! paid: ${customer_melons:.2f}, expected ${melon_cost:.2f}")
+            print(f"{customer_name} OVERPAID! paid: ${customer_paid:.2f}, expected ${customer_expected:.2f}")
+            #elif statement checks if the expected amount is less than amount customer paid
         elif customer_expected == customer_paid:
-            print(f"{customer_name} paid correctly! paid: ${melon_cost:.2f}")
-        
-        
-        
-        """if customer_expected != customer_paid:  
-            print(f"{customer_name} paid ${customer_melons:.2f}, expected ${melon_cost:.2f}")"""
+            print(f"{customer_name} paid correctly! paid: ${customer_paid:.2f}")
+            #elif statement checks if the expected amount equals amount customer paid
 
 customer_data("customer-orders.txt")
